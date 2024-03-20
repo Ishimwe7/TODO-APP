21d8aea35b79ff9f42ba8af442a5d62284780dc8
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-03-21 00:48:41 +0200

    added server configuration file

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const controller = require('./back-end/controllers/todoController');
+const userController = require('./back-end/controllers/userController');
+
+const app = express();
+const url = "mongodb+srv://nyanja-cyane:nyanja@cluster0.qmnp1kf.mongodb.net/<todo_db>?retryWrites=true&w=majority";
+
+async function connect() {
+    try {
+        await mongoose.connect(url);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.use(express.json());
+
+app.use('/todos', controller);
+app.use('/users', userController)
+
+connect();
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
