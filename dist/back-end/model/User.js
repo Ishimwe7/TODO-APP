9046379936e27999d8dd1062edda1a9472ddf3f7
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-03-22 09:39:15 +0200

    commit

---
+++ dist/back-end/model/User.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const Schema = mongoose_1.default.Schema;
+const userSchema = new Schema({
+    names: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+module.exports = mongoose_1.default.model("User", userSchema);
